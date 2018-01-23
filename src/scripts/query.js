2d3caf3763aa215a6a7ba427bb57cbd3dca257d8
kurtiskusz <kurtiskusz@gmail.com>
2018-01-23 13:39:25 -0500

    Finished results page with dummy articles

--- src/scripts/query.js
+++ src/scripts/query.js
@@ -2,3 +2,8 @@ var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
 
 var queryTerm = queryString.split("=")[1];
+
+document.title = queryTerm + " - Big Watson"
+
+var showingResults = document.getElementById("showing-results");
+showingResults.innerHTML = "Showing results for: <strong>" + queryTerm + "</strong>";
