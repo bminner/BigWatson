ca9329020102265e85d431dcac0d95c264965d68
nmeyer1089 <nicholasmeyer0@gmail.com>
2018-02-08 16:48:08 -0500

    created our NLC, analyze page censors pages

--- Timebox2/NLC/create_nlc.py
+++ Timebox2/NLC/create_nlc.py
@@ -15,21 +15,30 @@ from watson_developer_cloud import NaturalLanguageClassifierV1
 #   "classifier_id": "33badcx272-nlc-24212"
 # }
 
+# {
+#   "created": "2018-02-08T21:37:07.878Z",
+#   "language": "en",
+#   "status": "Training",
+#   "status_description": "The classifier instance is in its training phase, not yet ready to accept classify requests",
+#   "classifier_id": "719427x293-nlc-242",
+#   "url": "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/719427x293-nlc-242",
+#   "name": "Pos Neg Class"
+# }
 
-file_name = 'test-classifier.csv'
 
-def create_api():
-  nlu = NaturalLanguageClassifierV1(
-    username='4d49cfca-bcdc-4ed9-a673-5d561faac440',
-    password='GbojuhoOT5rG')
+def create_api(file_name):
+    nlu = NaturalLanguageClassifierV1(
+      username='4d49cfca-bcdc-4ed9-a673-5d561faac440',
+      password='GbojuhoOT5rG')
 
-  with open(file_name, 'rb') as training_data:
-    classifier = nlu.create_classifier(
-      training_data=training_data,
-      metadata=json.dumps({'name': 'Classifier Demo', 'language': 'en'})
-    )
+    with open(file_name, 'rb') as training_data:
+        classifier = nlu.create_classifier(
+            training_data=training_data,
+            metadata=json.dumps({'name': 'Pos Neg Class', 'language': 'en'})
+        )
 
-  print(json.dumps(classifier, indent=2))
+    print(json.dumps(classifier, indent=2))
+
 
 def create_curl():
 
@@ -48,8 +57,10 @@ def create_curl():
     print(r.status_code)
     print(json.dumps(r.json(), indent=2))
 
+
 def __main__():
-    create_api()
+    file_name = '../sentences/pos_neg_responses.csv'
+    create_api(file_name)
 
 __main__()
 
--- Timebox2/NLC/use_nlc.py
+++ Timebox2/NLC/use_nlc.py
@@ -3,9 +3,26 @@ __author__ = 'Nicholas'
 from watson_developer_cloud import NaturalLanguageClassifierV1
 import json
 
-natural_language_classifier = NaturalLanguageClassifierV1(
+
+def classify_sentence(text):
+    CLASSIFIER_ID = '719427x293-nlc-242'
+
+    natural_language_classifier = NaturalLanguageClassifierV1(
     username='4d49cfca-bcdc-4ed9-a673-5d561faac440',
     password='GbojuhoOT5rG')
 
-classes = natural_language_classifier.classify('33badcx272-nlc-24212', 'catastrophic')
-print(json.dumps(classes, indent=2))
+    #toy classifier
+    #classes = natural_language_classifier.classify('33badcx272-nlc-24212', 'catastrophic')
+
+    #real classifier
+    classes = natural_language_classifier.classify(CLASSIFIER_ID, text)
+    return json.dumps(classes, indent=2)
+
+
+def __main__():
+    text = "good"
+
+    print(classify_sentence(text))
+
+
+__main__()
