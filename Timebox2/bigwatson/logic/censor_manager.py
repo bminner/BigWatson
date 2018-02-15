d5358642beb75b9330589c69a372922c6653da65
kurtiskusz <kurtiskusz@gmail.com>
2018-02-15 15:06:12 -0500

    Added censoring on title/summary, moved body censoring logic to post-article-click

--- Timebox2/bigwatson/logic/censor_manager.py
+++ Timebox2/bigwatson/logic/censor_manager.py
@@ -13,21 +13,20 @@ def censor_results(discovery_results, good_class):
         # TODO call nlc analysis on different c_article components
 
         # add new article with changes
-        censored_article = censor_article(article, good_class)
+        censored_article = censor_title_and_summary(article, good_class)
         censored_articles.append(censored_article)
 
     return censored_articles
 
 ###
+### Censors article body.
 ### Start with an entire page, break it blocks to analyze, censor from there
 ###
-def censor_article(article, good_class):
+def censor_body(body, good_class):
     print(good_class)
     POSSIBLE_CLASSES = ['positive', 'negative']
 
-    censored = Article.Article.from_article(article)
-
-    sentences = censored.body.split('.')
+    sentences = body.split('.')
     for i in range(len(sentences)):
         sentence = sentences[i]
         cls = ''
@@ -55,5 +54,73 @@ def censor_article(article, good_class):
             sentences[i] = '<del>' + sentence + '</del>'
 
     censored_body = '. '.join(sentences)
-    censored.body = censored_body
+    return censored_body
+
+
+def censor_title_and_summary(article, good_class):
+    """ censors and returns an article's title and summary. """
+
+    print(good_class)
+    POSSIBLE_CLASSES = ['positive', 'negative']
+
+    censored = Article.Article.from_article(article)
+
+    summary_sents = censored.summary.split('.')
+    for i in range(len(summary_sents)):
+        sentence = summary_sents[i]
+        cls = ''
+        conf = -1
+
+        #fragments from splitting on periods
+        if len(sentence) > 0:
+            cls, conf = classify_sentence(sentence)
+            print(cls, conf)
+
+        #basically if nuetral and very confident
+        if good_class not in POSSIBLE_CLASSES:
+            if conf > .9:
+                try:
+                    print("Strong Feelings: " + sentence)
+                except UnicodeEncodeError:
+                    print("unicode shit")
+                summary_sents[i] = '<del>' + sentence + '</del>'
+        #if not what we're aiming for
+        elif cls != good_class and conf > .4:
+            try:
+                print("Bad hombre:" + sentence)
+            except UnicodeEncodeError:
+                print("unicode shit")
+            summary_sents[i] = '<del>' + sentence + '</del>'
+
+    title_sents = censored.title.split('.')
+    for i in range(len(title_sents)):
+        sentence = title_sents[i]
+        cls = ''
+        conf = -1
+
+        #fragments from splitting on periods
+        if len(sentence) > 0:
+            cls, conf = classify_sentence(sentence)
+            print(cls, conf)
+
+        #basically if nuetral and very confident
+        if good_class not in POSSIBLE_CLASSES:
+            if conf > .9:
+                try:
+                    print("Strong Feelings: " + sentence)
+                except UnicodeEncodeError:
+                    print("unicode shit")
+                title_sents[i] = '<del>' + sentence + '</del>'
+        #if not what we're aiming for
+        elif cls != good_class and conf > .4:
+            try:
+                print("Bad hombre:" + sentence)
+            except UnicodeEncodeError:
+                print("unicode shit")
+            title_sents[i] = '<del>' + sentence + '</del>'
+
+    censored_summary = '. '.join(summary_sents)
+    censored.summary = censored_summary
+    censored_title = '. '.join(title_sents)
+    censored.title = censored_title
     return censored
