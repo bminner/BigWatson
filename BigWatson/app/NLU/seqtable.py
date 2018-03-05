c7554194ffc012dd65c8afb82f16301cae8224b1
Brian Groenke <groenke.5@buckeyemail.osu.edu>
2018-03-05 16:03:48 -0500

    Fix nlu import issues; rename to 'analyzer'
    Fixes issues with NLU importing variables and submodules
    
    Renames 'nlu.py' to 'analyzer.py'
    
    Adds string representation to Entity
    
    Adds additional return results to seqtable.lookup

--- BigWatson/app/NLU/seqtable.py
+++ BigWatson/app/NLU/seqtable.py
@@ -7,7 +7,14 @@ class SeqTable:
             self.total_len += len(seq)
 
     def lookup(self, index):
-        """Returns the sequence in the table in which the given index lies."""
+        """Looks up the given index in the sequence table.
+
+            Returns a 3-tuple of: the sequence s in which 'index' lies, the
+            cumulative index at which s begins, and the relative location of
+            index in s (index - s_start)
+        """
+        assert index >= 0
+        assert index < self.total_len
         return self._lookup(index, self.seqs)
 
     def _lookup(self, index, seqs):
@@ -18,4 +25,4 @@ class SeqTable:
         elif index >= tlen + len(seq):
             return self._lookup(index, seqs[mid:])
         else:
-            return seq
+            return seq, tlen, index - tlen
