69594e3ece95df644a1d7e11beaf757c28454993
Brian Groenke <bgroe8@gmail.com>
2018-03-24 18:57:19 -0400

    Convert NLU mentions to word nodes in analyzer

--- BigWatson/app/NLU/analyzer.py
+++ BigWatson/app/NLU/analyzer.py
@@ -1,8 +1,10 @@
 from watson_developer_cloud.natural_language_understanding_v1 import *
 from ..models import Article
+from ..logic.doctree import DocTree, LinkedIndex
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 import json
 
+MIN_TEXT_LENGTH = 50
 
 def init_nlu_engine():
     url = "https://gateway.watsonplatform.net/natural-language-understanding/api",
@@ -16,9 +18,21 @@ def init_nlu_engine():
 
 nlu = init_nlu_engine()
 
-def analyze(text):
-    """Analyzes the given text and returns a generator of Entity objects."""
-    response = nlu.analyze(
+def analyze(doctree):
+    """ Analyzes the given DocTree and returns a generator of Entity objects. """
+    assert(isinstance(doctree, DocTree))
+
+    title = doctree.get_title()
+    summary = doctree.get_summary()
+    body = doctree.get_body()
+    title_entities = _parse_entities(_query_nlu(title), doctree.title_word_at) if len(title) >= MIN_TEXT_LENGTH else []
+    summary_entities = _parse_entities(_query_nlu(summary), doctree.summary_word_at) if len(summary) >= MIN_TEXT_LENGTH else []
+    body_entities = _parse_entities(_query_nlu(body), doctree.body_word_at) if len(body) >= MIN_TEXT_LENGTH else []
+
+    return AnalyzeResult(title_entities, summary_entities, body_entities)
+
+def _query_nlu(text):
+    return nlu.analyze(
         text=text,
         features=Features(
             entities=EntitiesOptions(
@@ -29,17 +43,24 @@ def analyze(text):
         )
     )
 
-    return _parse_entities(response)
-
-
-def _parse_entities(response):
+def _parse_entities(response, word_lookup_func):
     entities = response['entities'] if 'entities' in response else []
     for e in entities:
         name = e['text']
         ttype = e['type']
         score = e['sentiment']['score']
-        mentions = [(m['text'], m['location']) for m in e['mentions']] if 'mentions' in e else []
+        mentions = [_parse_mention(m, word_lookup_func) for m in e['mentions']] if 'mentions' in e else []
         yield Entity(name, ttype, score, mentions)
+
+def _parse_mention(m, word_lookup_func):
+    text = m['text']
+    location = m['location']
+    word = word_lookup_func(location[0])
+    assert(word is not None) # This should not happen; implies that NLU gave us a bad location value
+    return (text, word)
+
+class AnalyzeResult:
+    def __init__(self, title_entities, summary_entities, body_entitites):
 
 
 class Entity:
