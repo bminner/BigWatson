1962aee9d384a517d45b9ae618240cfc4bfd2ebc
kurtiskusz <kurtiskusz@gmail.com>
2018-04-10 00:29:21 -0400

    added try it yourself section for custom input

--- BigWatson/app/urls.py
+++ BigWatson/app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('results/', views.results, name='results'),
     path('result/', views.result, name='result'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('tryit/', views.tryit, name='tryit'),
+    path('tryit_results/', views.tryit_results, name='tryit_results'),
 ]
